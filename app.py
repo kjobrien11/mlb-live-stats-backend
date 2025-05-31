93fc4901a7274ca943a239cb2fbcefbaeede9307
KJ OBrien <kjokjo219@gmail.com>
2025-05-31 17:06:36 -0400

    improves clarify of function calls with parameters and returns

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import statsapi
 from datetime import date
 
-
-
-def get_line_scores():
+def get_line_scores(game_pks):
+    line_scores = []
     for game in game_pks:
         line_scores.append(statsapi.linescore(game))
+    return line_scores
 
-def print_line_scores():
+def print_line_scores(line_scores):
     for game in line_scores:
         print(game)
         print()
@@ -16,7 +16,6 @@ if __name__ == "__main__":
     today = date.today()
     todays_schedule = statsapi.schedule(date=today)
     game_pks = [game['game_id'] for game in todays_schedule]
-    line_scores = []
-    get_line_scores()
-    print_line_scores()
     
+    line_scores = get_line_scores(game_pks)
+    print_line_scores(line_scores)
