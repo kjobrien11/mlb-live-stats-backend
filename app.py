5dbac4d717e972adb29f49565361f0bf9508b5d7
KJ OBrien <kjokjo219@gmail.com>
2025-05-31 17:04:26 -0400

    formats app.py to have main and adds function names

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 import statsapi
 from datetime import date
 
-today = date.today()
-todays_schedule = statsapi.schedule(date=today)
-game_pks = [game['game_id'] for game in todays_schedule]
-print(game_pks)
 
-for game in game_pks:
-    print(statsapi.linescore(game))
-    print()
+
+def get_line_scores():
+    for game in game_pks:
+        line_scores.append(statsapi.linescore(game))
+
+def print_line_scores():
+    for game in line_scores:
+        print(game)
+        print()
+
+if __name__ == "__main__":
+    today = date.today()
+    todays_schedule = statsapi.schedule(date=today)
+    game_pks = [game['game_id'] for game in todays_schedule]
+    line_scores = []
+    get_line_scores()
+    print_line_scores()
+    
