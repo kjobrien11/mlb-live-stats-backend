4ebdd20ab5aa27d1219b62a53eaa613b1d92e73c
KJ OBrien <kjokjo219@gmail.com>
2025-05-31 17:40:30 -0400

    updates the app to have a server in the backend to return the values to a front end

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
 import statsapi
+from fastapi import FastAPI
 from datetime import date
 
-def get_line_scores(game_pks):
+app = FastAPI()
+
+@app.on_event("startup")
+async def startup_event():
+    today = date.today()
+    todays_schedule = statsapi.schedule(date=today)
+    app.state.game_pks = [game['game_id'] for game in todays_schedule]
+    app.state.line_scores = []
+
+def retrieve_line_scores(game_pks):
     line_scores = []
     for game in game_pks:
         line_scores.append(statsapi.linescore(game))
     return line_scores
 
-def print_line_scores(line_scores):
-    for game in line_scores:
-        print(game)
-        print()
-
-if __name__ == "__main__":
-    today = date.today()
-    todays_schedule = statsapi.schedule(date=today)
-    game_pks = [game['game_id'] for game in todays_schedule]
-    
-    line_scores = get_line_scores(game_pks)
-    print_line_scores(line_scores)
+@app.get("/line-scores")
+def get_line_scores():
+    app.state.line_scores = retrieve_line_scores(app.state.game_pks)
+    return app.state.line_scores
