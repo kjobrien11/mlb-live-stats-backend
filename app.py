acd2ed9184723dc1bb0f02aabb79f0ad57b1dd03
KJ OBrien <kjokjo219@gmail.com>
2025-05-31 16:58:45 -0400

    prints line scores of all games today

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ today = date.today()
 todays_schedule = statsapi.schedule(date=today)
 game_pks = [game['game_id'] for game in todays_schedule]
 print(game_pks)
+
+for game in game_pks:
+    print(statsapi.linescore(game))
+    print()
